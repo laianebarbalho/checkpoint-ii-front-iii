c03d0131e3a0c38aac6bacf4ffaf6a2718f0c155
Juliana Novais <juliana.novais@icloud.com>
2023-04-15 11:18:56 -0300

    Autenticacao no fomulario de login

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,23 +1,31 @@
 import styles from "./Form.module.css";
+import { useState } from "react";
+import { useAuth } from "../hooks/auth";
 
 const LoginForm = () => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const { signIn } = useAuth();
   const handleSubmit = (e) => {
     //Nesse handlesubmit você deverá usar o preventDefault,
-    //enviar os dados do formulário e enviá-los no corpo da requisição 
+    //enviar os dados do formulário e enviá-los no corpo da requisição
     //para a rota da api que faz o login /auth
     //lembre-se que essa rota vai retornar um Bearer Token e o mesmo deve ser salvo
     //no localstorage para ser usado em chamadas futuras
     //Com tudo ocorrendo corretamente, o usuário deve ser redirecionado a página principal,com react-router
     //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
+    e.preventDefault();
+    signIn({
+      username: login,
+      password,
+    });
   };
 
   return (
     <>
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
-      <div
-        className={`text-center card container ${styles.card}`}
-      >
+      <div className={`text-center card container ${styles.card}`}>
         <div className={`card-body ${styles.CardBody}`}>
           <form onSubmit={handleSubmit}>
             <input
@@ -25,6 +33,7 @@ const LoginForm = () => {
               placeholder="Login"
               name="login"
               required
+              onChange={(event) => setLogin(event.target.value)}
             />
             <input
               className={`form-control ${styles.inputSpacing}`}
@@ -32,6 +41,7 @@ const LoginForm = () => {
               name="password"
               type="password"
               required
+              onChange={(event) => setPassword(event.target.value)}
             />
             <button className="btn btn-primary" type="submit">
               Send
